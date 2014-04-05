344336179a5d47764a0c642e0e70a849012bb7b7
Peter Kröner <peter@peterkroener.de>
2014-04-05 18:20:59 +0200

    Anforderungen und Fail-Tests für renderVideo

--- script/renderVideo.js
+++ script/renderVideo.js
@@ -1,11 +1,24 @@
+/*
+  Anforderungen
+  -------------
+  * renderVideo(sourceUrl) stellt ein Video bereit, das das unter "sourceUrl"
+    hinterlegte Video abspielt
+  * renderVideo(sourceUrl) gibt ein Promise auf ein abspielbereites
+    Video-Element zurück
+    * wenn das Video geladen werden kann und das Element einen abspielbereiten
+      Zustand erreicht, wird das Promise mit dem Video-Element aufgelöst
+    * gibt es mit dem Video ein Problem, wird das Promise mit einem Error
+      rejected
+*/
+
 define(['jquery', 'q'], function($, Q){
 
-  return function renderVideo(source){
+  return function renderVideo(sourceUrl){
 
     var deferred = Q.defer();
 
     $('<video>')
-      .attr('src', source)
+      .attr('src', sourceUrl)
       .on({
         canplay: function onVideoCanPlay(evt){
           deferred.resolve(evt.target);
