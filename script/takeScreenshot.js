a254826ee2146ab5faf82fea2e90512bc306033d
Peter Kröner <peter@peterkroener.de>
2014-04-05 19:15:24 +0200

    Fail-Tests für takeScreenshot

--- script/takeScreenshot.js
+++ script/takeScreenshot.js
@@ -3,12 +3,14 @@
   -------------
   * takeScreenshot(quelle) erstellt einen Screenshot des Elements "quelle"
     * wird "quelle" nicht angegeben, wirft die Funktion einen Error
+    * ist "quelle" kein Canvas-, Img-, oder Video-Element, wirft die Funktion
+      einen Error
   * takeScreenshot(quelle) gibt ein Promise zurück
-    * ist "quelle" kein Canvas-, Img-, oder Video-Element, wird das Promise mit
-      einem Error rejected
     * bei erfolgreichem Screenshot wird das Promise mit dem Screenshot als
       Blob-Objekt aufgelöst
-    * der Screenshot hat die gleichen Maße wie das Element "quelle"
+      * der Screenshot hat die gleichen Maße wie das Element "quelle"
+    * bei Fehler während des Screenshot-Tests wird das Promise mit einem Error
+      rejected
 */
 
 define(['jquery', 'q'], function($, Q){
@@ -19,34 +21,34 @@ define(['jquery', 'q'], function($, Q){
       throw new Error('Keine Screenshot-Quelle angegeben');
     }
 
+    if(!$(source).is('canvas, video, img')){
+      throw new Error('Kann Quelle nicht verarbeiten');
+    }
+
     var deferred = Q.defer();
     var $canvas = $('<canvas>');
     var context = $canvas.get(0).getContext('2d');
 
-    if(!$(source).is('canvas, video, img')){
-      var err = new Error('Kann Quelle nicht verarbeiten');
-      deferred.reject(err);
-    }
 
-    else {
+    var sourceWidth = $(source).get(0).naturalWidth ||
+      $(source).get(0).videoWidth ||
+      $(source).attr('width');
+    var sourceHeight = $(source).get(0).naturalHeight ||
+      $(source).get(0).videoHeight ||
+      $(source).attr('height');
 
-      var sourceWidth = $(source).get(0).naturalWidth ||
-        $(source).get(0).videoWidth ||
-        $(source).attr('width');
-      var sourceHeight = $(source).get(0).naturalHeight ||
-        $(source).get(0).videoHeight ||
-        $(source).attr('height');
+    $canvas.attr({
+      width: sourceWidth,
+      height: sourceHeight
+    });
 
-      $canvas.attr({
-        width: sourceWidth,
-        height: sourceHeight
-      });
-
+    try {
       context.drawImage($(source).get(0), 0, 0);
       $canvas.get(0).toBlob(function(blob){
         deferred.resolve(blob);
       });
-
+    } catch(e){
+      deferred.reject(e);
     }
 
     return deferred.promise;
